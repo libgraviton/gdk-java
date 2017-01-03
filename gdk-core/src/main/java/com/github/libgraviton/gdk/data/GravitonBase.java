69a07a2c301e481be8ff08cec2534741534a243a
Michael Wegener <michael.wegener@swisscom.com>
2017-01-03 08:30:30 +0100

    add GravitonBase javadoc

--- gdk-core/src/main/java/com/github/libgraviton/gdk/data/GravitonBase.java
+++ gdk-core/src/main/java/com/github/libgraviton/gdk/data/GravitonBase.java
@@ -1,7 +1,11 @@
 package com.github.libgraviton.gdk.data;
 
 /**
- * Created by taawemi6 on 02/12/16.
+ * Base interface to be implemented by all generated GDK classes.
+ *
+ * @author List of contributors {@literal <https://github.com/libgraviton/gdk-java/graphs/contributors>}
+ * @see <a href="http://swisscom.ch">http://swisscom.ch</a>
+ * @version $Id: $Id
  */
 public interface GravitonBase {
 
