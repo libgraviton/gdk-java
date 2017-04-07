95fe2605344a761795b6c42614d000690b5ab172
Michael Wegener <michael.wegener@swisscom.com>
2017-04-07 13:20:24 +0200

    clean imports

--- gdk-core/src/main/java/com/github/libgraviton/gdk/RequestExecutor.java
+++ gdk-core/src/main/java/com/github/libgraviton/gdk/RequestExecutor.java
@@ -11,7 +11,6 @@ import com.github.libgraviton.gdk.exception.UnsuccessfulResponseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Arrays;
 
 /**
